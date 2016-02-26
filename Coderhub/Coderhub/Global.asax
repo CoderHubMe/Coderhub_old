de16d4e2f815f8be1b3a8a1a2cf6ec3d05f05786
ACPPPF <acpppf@mail.missouri.edu>
2016-02-25 19:22:08 -0600

    Started a .NET application for us to use

---
+++ Coderhub/Coderhub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Coderhub.MvcApplication" Language="C#" %>
---
+++ Coderhub/Coderhub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Coderhub.Startup))]
+namespace Coderhub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
